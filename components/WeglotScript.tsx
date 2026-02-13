21009520b35fe1fd895cfeec6c6e903d76bfa22b
Mustafa <86mussai@skola.boras.se>
2026-02-13 16:57:48 +0100

    updateWeglot

--- components/WeglotScript.tsx
+++ components/WeglotScript.tsx
@@ -1,4 +1,4 @@
-"use client"
+/* "use client"
 
 import Script from "next/script"
 
@@ -22,6 +22,49 @@ export default function WeglotScript() {
           })
         }
       }}
+    />
+  )
+}
+ */
+
+"use client"
+
+import Script from "next/script"
+import { usePathname } from "next/navigation"
+import { useEffect } from "react"
+
+declare global {
+  interface Window {
+    Weglot?: any
+  }
+}
+
+export default function WeglotScript() {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    if (window.Weglot) {
+      window.Weglot.initialize({
+        api_key: "wg_c94a0dce3d0d4ace070de57d7352b0304",
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (window.Weglot && window.Weglot.initialized) {
+        window.Weglot.reload()
+        clearInterval(interval)
+      }
+    }, 300)
+
+    return () => clearInterval(interval)
+  }, [pathname])
+
+  return (
+    <Script
+      src="https://cdn.weglot.com/weglot.min.js"
+      strategy="afterInteractive"
     />
   )
 }
