7b8cd16ae7c034849fe37c7aafc7e1a3ab9b64af
Mustafa <86mussai@skola.boras.se>
2026-02-13 14:16:40 +0100

    updateAgian

--- app/(site)/ListingDetail/page.tsx
+++ app/(site)/ListingDetail/page.tsx
@@ -5,7 +5,7 @@ import { useSearchParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "@/api/supabaseClient";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { Textarea } from "@/components/ui/textarea";
 import {
   ChevronLeft,
