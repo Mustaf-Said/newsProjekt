195a44c0ae28f100dea90d675eb48ab64756659a
Mustafa <86mussai@skola.boras.se>
2026-02-13 19:01:46 +0100

    updateWeglotKey

--- components/WeglotScript.tsx
+++ components/WeglotScript.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import Script from "next/script"
-import { usePathname } from "next/navigation"
-import { useEffect } from "react"
 
 declare global {
   interface Window {
@@ -11,21 +9,13 @@ declare global {
 }
 
 export default function WeglotScript() {
-  const pathname = usePathname()
-
-  useEffect(() => {
-    if (window.Weglot && window.Weglot.refresh) {
-      window.Weglot.refresh()
-    }
-  }, [pathname])
-
   return (
     <Script
       src="https://cdn.weglot.com/weglot.min.js"
       strategy="afterInteractive"
       onLoad={() => {
         window.Weglot.initialize({
-          api_key: 'wg_198fe66593c57ca32991fd1e418925e92',
+          api_key: "wg_198fe66593c57ca32991fd1e418925e9"
         })
       }}
     />
